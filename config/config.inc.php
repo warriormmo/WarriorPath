dd01f85f4bece7a8212fabbac162a53b86057c70
greezlee <nazarov.mihail@gmail.com>
2009-06-21 11:33:05 +0400

    create common config file / add macine relative to gitignore

---
+++ config/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+  //project relative defines
+  define('PROJECT_TITLE', 'WarriorPath');
+
+  //db defines
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'phpmmorpg');
+
+  //cookie defines
+  define('cookie_prefix', 'wp_');
+  define('cookie_userid', cookie_prefix.'userid');
+?>
