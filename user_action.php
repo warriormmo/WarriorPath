7c7772c811fdba3929c6b6850dc68f5ad6a85aec
greezlee <nazarov.mihail@gmail.com>
2009-06-22 00:10:11 +0400

    user_action: reg and auth actions

--- user_action.php
+++ user_action.php
@@ -1,5 +1,6 @@
 <?php
   require_once('include/commons.inc');
+  require_once('libs/user.lib');
 
   $action = @$_GET['action'];
 
@@ -7,9 +8,9 @@
 
   switch($action){
     case 'reg':
-      $user_nick = @$_POST['nick'];
-      $user_email = @$_POST['email'];
-      $user_password = @$_POST['password'];
+      $user_nick = @$_REQUEST['nick'];
+      $user_email = @$_REQUEST['email'];
+      $user_password = @$_REQUEST['password'];
 
       if (!$user_nick) $error .= "\nОтсутвует имя пользователя!";
       if (!$user_email) $error .= "\nОтсутвует Email пользователя!";
@@ -19,22 +20,53 @@
         $user = usr_getUser(db_relation(
           db_field('nick', DB_EQU, $user_nick),
           DB_OR,
-          db_field('email', DB_EQU, $user_password);
+          db_field('email', DB_EQU, $user_email)
         ));
+
         if ($user){
-          if ($user['nick'] == $user_nick) $error = 'Пользователь с таким ником уже присутствует в игре';
-          if ($user['email'] == $user_email) $error = 'Пользователь с таким адресом E-mail уже присутствует в игре';
+          if (strtolower($user['nick']) == strtolower($user_nick)) $error = 'Пользователь с таким ником уже присутствует в игре';
+          if (strtolower($user['email']) == strtolower($user_email)) $error = 'Пользователь с таким адресом E-mail уже присутствует в игре';
         } else {
           $res = usr_insertUser(array(
             'nick' => $user_nick,
             'email' => $user_email,
             'password' => md5(md5($user_password)),
            ));
-           $error = $res ? 0 : 'Внутенняя ошибка сервера.',
+           $error = $res ? 0 : 'Внутенняя ошибка сервера.';
         }
+      }
+    break;
+    case 'auth':
+      $error = 0;
+      $user_nick = @$_REQUEST['nick'];
+      $user_password = @$_REQUEST['password'];
+
+      if (!$user_nick) $error .= "\nОтсутвует имя пользователя!";
+      if (!$user_password) $error .= "\nОтсутвует пароль пользователя!";
+
+      if ($error == 0){
+        $user = usr_getUser(array(
+          'nick' => $user_nick
+        ));
+        if (!$user){
+          $error = 'Пользователь с таким ником не найден.';
+        } else {
+          if ($user['password'] != md5(md5($user_password))){
+            $error = 'Неверный пароль';
+          }
+        }
+      }
+      if ($error == 0){
+        setcookie(cookie_userid, $user['id'], 0, '/');
+        usr_updateUser(array('lastvisit_date' => time()), $user['id']);
+      } else {
+        setcookie(cookie_userid, 0, 0, '/');
       }
     break;
   }
 
+  $error = ($error === '') ? 'Неверный код действия.' : $error;
+
   echo cmn_generateResultXML($error);
+  return;
 ?>
