8cc41e14f97a6ea44920cb8adb8feb585aaa5918
greezlee <nazarov.mihail@gmail.com>
2009-06-24 01:38:47 +0400

    добавил префикс, добавил пару запятых в комменты
    префиксы - чтобы отличать от системных комманд

--- stuff/scritps/wptestmerge.sh
+++ stuff/scritps/wptestmerge.sh
@@ -3,10 +3,10 @@
 LOG_FILE=$HOME"/.git.logs"
 
 if [ "$1" = "help" ]; then
-  echo "Использование: mastermerge.sh [branch|help]"
+  echo "Использование: testmerge.sh [branch|help]"
   echo ""
   echo "  help - выводит этот текст."
-  echo "  branch - имя ветки которую надо вмержить в мастер"
+  echo "  branch - имя ветки, которую надо вмержить в тест"
   exit
 fi
 
@@ -15,9 +15,9 @@ if [ -n "$1" ]; then
   echo -n "Ветка $branch присутствует?.."
   if [ `git branch | grep $branch | wc -c` -gt 0 ]; then
     echo "Да"
-    echo -n "Переходим в ветку master..."
-    git checkout master 1>>$LOG_FILE 2>>$LOG_FILE && echo "ОК" || echo "Ошибка"
-    echo -n "Вмерживаем ветку $branch в локальный master..."
+    echo -n "Переходим в ветку test..."
+    git checkout test 1>>$LOG_FILE 2>>$LOG_FILE && echo "ОК" || echo "Ошибка"
+    echo -n "Вмерживаем ветку $branch в локальный test..."
     git merge $branch 1>>$LOG_FILE 2>>$LOG_FILE && echo "Ок" || echo "Ошибка"
   else
     echo "Нет"
