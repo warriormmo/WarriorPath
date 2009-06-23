dc24c3cb49d8ef809b3c13a6ae0cda54f4a9ddcf
greezlee <nazarov.mihail@gmail.com>
2009-06-24 00:42:25 +0400

    пишем все в лог

--- stuff/scritps/update.sh
+++ stuff/scritps/update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+LOG_FILE=$HOME"/.git.logs"
+
 if [ "$1" = "help" ]; then
-  echo "Использование: update.sh [reset|test]"
+  echo "Использование: update.sh [reset|help]"
   echo ""
   echo "  help - выводит этот текст."
   echo "  reset - сбрасывает локальные master и test к удаленным (без указания параметра - мержит)"
@@ -9,24 +11,24 @@ if [ "$1" = "help" ]; then
 fi
 
 echo -n "Забираем последние имзменения из репозитория"
-git fetch -t origin || echo "... Ошибка" && echo "...Ок"
+git fetch -t origin 1>>$LOG_FILE 2>>$LOG_FILE || echo "... Ошибка" && echo "...Ок"
 
 if [ "$1" = "reset" ]; then
   echo -n "Переходим в ветку master"
-  git checkout master || echo "... Ошибка" && echo "...Ок"
+  git checkout master 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
   echo -n "Сбрасываем локальный master к удаленному"
-  git reset --hard origin/master || echo "... Ошибка" && echo "...Ок"
+  git reset --hard origin/master 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
   echo -n "Переходим в ветку test"
-  git checkout test || echo "... Ошибка" && echo "...Ок"
+  git checkout test 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
   echo -n "Сбрасываем локальный test к удаленному"
-  git reset --hard origin/test || echo "... Ошибка" && echo "...Ок"
+  git reset --hard origin/test 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
 else
   echo -n "Переходим в ветку master"
-  git checkout master 1>>/dev/null 2>>/dev/null || echo "... Ошибка" && echo "...Ок"
+  git checkout master 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
   echo -n "Вмерживаем удаленный master в локальный"
-  git merge origin/master 1>>/dev/null 2>>/dev/null || echo "... Ошибка" && echo "...Ок"
+  git merge origin/master 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
   echo -n "Переходим в ветку test"
-  git checkout test 1>>/dev/null 2>>/dev/null || echo "... Ошибка" && echo "...Ок"
+  git checkout test 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
   echo -n "Вмерживаем удаленный test в локальный"
-  git merge origin/test 1>>/dev/null 2>>/dev/null || echo "... Ошибка" && echo "...Ок"
+  git merge origin/test 1>>$LOG_FILE 2>>$LOG_FILE && echo "...Ок" || echo "... Ошибка"
 fi
