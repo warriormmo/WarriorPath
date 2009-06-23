3ec29f16d5cd303a9b511f06cdf7c89a632daa8f
greezlee <nazarov.mihail@gmail.com>
2009-06-24 00:58:32 +0400

    скипт для мержа веток в мастер

---
+++ stuff/scritps/mastermerge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LOG_FILE=$HOME"/.git.logs"
+
+if [ "$1" = "help" ]; then
+  echo "Использование: mastermerge.sh [branch|help]"
+  echo ""
+  echo "  help - выводит этот текст."
+  echo "  branch - имя ветки которую надо вмержить в мастер"
+  exit
+fi
+
+if [ -n "$1" ]; then
+  branch="$1"
+  echo -n "Ветка $branch присутствует?.."
+  if [ `git branch | grep $branch | wc -c` -gt 0 ]; then
+    echo "Да"
+    echo -n "Переходим в ветку master..."
+    git checkout master 1>>$LOG_FILE 2>>$LOG_FILE && echo "ОК" || echo "Ошибка"
+    echo -n "Вмерживаем ветку $branch в локальный master..."
+    git merge $branch 1>>$LOG_FILE 2>>$LOG_FILE && echo "Ок" || echo "Ошибка"
+  else
+    echo "Нет"
+    echo "Указанная вами ветка не существует. Проверте правильность написания имени ветки."
+  fi
+fi
