87374862b706a49d0382dca45e86fe7476d44d9c
Michaael <michaael@narod.ru>
2009-06-24 14:49:57 +0400

    поменял для себя кодировку.... думаю не стоит их куданить вмерживать

--- stuff/scritps/wptestmerge.sh
+++ stuff/scritps/wptestmerge.sh
@@ -3,24 +3,24 @@
 LOG_FILE=$HOME"/.git.logs"
 
 if [ "$1" = "help" ]; then
-  echo "РСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ: testmerge.sh [branch|help]"
+  echo "Использование: testmerge.sh [branch|help]"
   echo ""
-  echo "  help - РІС‹РІРѕРґРёС‚ СЌС‚РѕС‚ С‚РµРєСЃС‚."
-  echo "  branch - РёРјСЏ РІРµС‚РєРё, РєРѕС‚РѕСЂСѓСЋ РЅР°РґРѕ РІРјРµСЂР¶РёС‚СЊ РІ С‚РµСЃС‚"
+  echo "  help - выводит этот текст."
+  echo "  branch - имя ветки, которую надо вмержить в тест"
   exit
 fi
 
 if [ -n "$1" ]; then
   branch="$1"
-  echo -n "Р’РµС‚РєР° $branch РїСЂРёСЃСѓС‚СЃС‚РІСѓРµС‚?.."
+  echo -n "Ветка $branch присутствует?.."
   if [ `git branch | grep $branch | wc -c` -gt 0 ]; then
-    echo "Р”Р°"
-    echo -n "РџРµСЂРµС…РѕРґРёРј РІ РІРµС‚РєСѓ test..."
-    git checkout test 1>>$LOG_FILE 2>>$LOG_FILE && echo "РћРљ" || echo "РћС€РёР±РєР°"
-    echo -n "Р’РјРµСЂР¶РёРІР°РµРј РІРµС‚РєСѓ $branch РІ Р»РѕРєР°Р»СЊРЅС‹Р№ test..."
-    git merge $branch 1>>$LOG_FILE 2>>$LOG_FILE && echo "РћРє" || echo "РћС€РёР±РєР°"
+    echo "Да"
+    echo -n "Переходим в ветку test..."
+    git checkout test 1>>$LOG_FILE 2>>$LOG_FILE && echo "ОК" || echo "Ошибка"
+    echo -n "Вмерживаем ветку $branch в локальный test..."
+    git merge $branch 1>>$LOG_FILE 2>>$LOG_FILE && echo "Ок" || echo "Ошибка"
   else
-    echo "РќРµС‚"
-    echo "РЈРєР°Р·Р°РЅРЅР°СЏ РІР°РјРё РІРµС‚РєР° РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚. РџСЂРѕРІРµСЂС‚Рµ РїСЂР°РІРёР»СЊРЅРѕСЃС‚СЊ РЅР°РїРёСЃР°РЅРёСЏ РёРјРµРЅРё РІРµС‚РєРё."
+    echo "Нет"
+    echo "Указанная вами ветка не существует. Проверте правильность написания имени ветки."
   fi
 fi
